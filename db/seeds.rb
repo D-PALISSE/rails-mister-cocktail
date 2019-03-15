1068fe9ece7bef1fda8ff6ad7497d9ee093d4880
David Palisse <palisse.david44@gmail.com>
2019-03-15 14:13:01 +0100

    seed cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+Cocktail.create!(name: "Corcovado", image: "http://www.habanera.gr/images/bar/corcovando.jpg" )
+Cocktail.create!(name: "Spritz", image: "https://img.static-rmg.be/a/view/q75/w1092/h615/1994032/spritz-jpg.jpg" )
+Cocktail.create!(name: "MOjito", image: "https://cache.marieclaire.fr/data/photo/w1000_ci/54/mojito.jpg")
