97ff9c5512bba5f5bdaca5f9d8698660b1dcd327
David Palisse <palisse.david44@gmail.com>
2019-03-16 21:46:15 +0100

    done validation for image or photo upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,19 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
+  mount_uploader :photo, PhotoUploader
+
+  validates :image, presence: true, allow_nil: true
+  validates :photo, presence: true, allow_nil: true
   validates :name, presence: true, uniqueness: true
+
+  validate :image_xor_photo
+
+  private
+
+  def image_xor_photo
+    unless image.blank? ^ photo.blank?
+      errors.add(:base, "Specify a image url or upload a photo, not both but at least one")
+    end
+  end
 end
