79f41c7919a4c1031b5539b71bdeb593dfa111f8
David Palisse <palisse.david44@gmail.com>
2019-03-16 23:36:24 +0100

    validations photos removed

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,17 +4,5 @@ class Cocktail < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
-  validates :image, presence: true, allow_nil: true
-  validates :photo, presence: true, allow_nil: true
   validates :name, presence: true, uniqueness: true
-
-  validate :image_xor_photo
-
-  private
-
-  def image_xor_photo
-    unless image.blank? ^ photo.blank?
-      errors.add(:base, "Specify a image url or upload a photo, not both but at least one")
-    end
-  end
 end
